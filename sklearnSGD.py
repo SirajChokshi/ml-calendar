0eb93fd9620711cb73b162b4139fd84b963bdcb2
Atharva Kasar <akasar@seas.upenn.edu>
2020-05-26 20:46:54 -0400

    Adding Polynomial Feature

--- sklearnSGD.py
+++ sklearnSGD.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[115]:
+# In[25]:
 
 
 # imports
 from sklearn import preprocessing
-import numpy as np
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import SGDRegressor
 from sklearn import preprocessing
+from sklearn.preprocessing import PolynomialFeatures 
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from numpy import random
 from sklearn.model_selection import train_test_split
@@ -27,19 +28,18 @@ y = list(dataset['val'])
 print(TRAIN_TEST_SEED)
 
 
-# In[116]:
+# In[26]:
 
 
 # format all independent values
 X = np.transpose([x1, x2])
 
 # X is a matrix with all of the independent variable values
-# print(X)
 
 print(X.shape)
 
 
-# In[117]:
+# In[27]:
 
 
 # split data into training/testing
@@ -54,8 +54,13 @@ print(X_test.shape)
 y_test = np.array(y_test)
 print(y_test.shape)
 
-# print(X_train)
+# construct Polynomial object
+poly = PolynomialFeatures(degree=3)
 
+# transform for polynomial fit
+X_train = poly.fit_transform(X_train)
+
+X_test = poly.fit_transform(X_test)
 
 # standardizing data
 scaler = preprocessing.StandardScaler().fit(X_train)
@@ -63,7 +68,7 @@ X_train = scaler.transform(X_train)
 X_test=scaler.transform(X_test)
 
 
-# In[118]:
+# In[28]:
 
 
 gradient = SGDRegressor()
@@ -72,12 +77,10 @@ gradient.fit(X_train, y_train)
 
 gradient_predictions = gradient.predict(X_test)
 
-# print(gradient_predictions)
-
 gradient_errors = []
 
 
-# In[119]:
+# In[29]:
 
 
 gradient.predict(X_test)
@@ -92,20 +95,27 @@ print('Mean Squared Error :',mean_squared_error(y_test, gradient.predict(X_test)
 print('Mean Absolute Error :',mean_absolute_error(y_test, gradient.predict(X_test)))
 
 
-# In[120]:
+# In[30]:
 
 
 # SkLearn SGD classifier predicted weight matrix
 sklearn_w=gradient.coef_
 sklearn_w
 
-# Calculating the differences between predicted and actual
+
+# In[31]:
+
+
+# print predicted values (truncated) vs actual values
+print("{:9s} {:9s} {}".format("Predicted", "Actual", "% Error"))
 for row in range(0, len(gradient_predictions)):
 	percent_error = abs((gradient_predictions[row] - y_test[row])/gradient_predictions[row] * 100)
 	gradient_errors.append(percent_error)
+	# print("{:9s} {:9s} {:5f}".format(str(int(gradient_predictions[row])), str(y_test[row]), percent_error))
 
 # print average percent error
 print("\n{:19s} {:5f}".format("Avg % Error:", np.average(gradient_errors)))
 
 print("Confusion/Covariance Matrix")
 print(np.corrcoef(gradient.predict(X_test), y_test))
+
